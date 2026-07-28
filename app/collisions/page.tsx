c9428b8f9397c59ab7e6ba4c2b1ef647ee4886e8
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2026-07-28 16:59:22 +0700

    calling collisions const

--- app/collisions/page.tsx
+++ app/collisions/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Matter from "matter-js";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 type BallData = {
   body: Matter.Body;
@@ -9,4 +9,16 @@ type BallData = {
   id: number;
 };
 
-export default function Page() {}
+export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const engineRef = useRef<Matter.Engine | null>(null);
+  const worldRef = useRef<Matter.World | null>(null);
+  const renderRef = useRef<Matter.Render | null>(null);
+  const wallsRef = useRef<Matter.Body[]>([]);
+  const ballsRef = useRef<BallData[]>([]);
+  const draggingBall = useRef<Matter.Body | null>(null);
+  const dragStart = useRef({ x: 0, y: 0 });
+  const dragCurrent = useRef({ x: 0, y: 0 });
+  const [ballCount, setBallCount] = useState(2);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+}
