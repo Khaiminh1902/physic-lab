e966124dd6be535b203da5b96687d35190c5d789
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2026-08-03 08:50:28 +0700

    defining const for particles simulation

--- app/particles/page.tsx
+++ app/particles/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import Matter from "matter-js";
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const page = () => {
+export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const engineRef = useRef<Matter.Engine | null>(null);
+  const particlesRef = useRef<Matter.Body[]>([]);
+  const worldRef = useRef<Matter.World | null>(null);
+  const [gravity, setGravity] = useState(1);
+  const [restitution, setRestitution] = useState(0.95);
+  const [particleCount, setParticleCount] = useState(500);
+
   return (
     <div className="relative h-screen w-screen overflow-hidden">
       <Link href="/" className="absolute left-4 top-4 z-10 font-semibold">
@@ -9,6 +19,4 @@ const page = () => {
       </Link>
     </div>
   );
-};
-
-export default page;
+}
